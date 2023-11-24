7a24111028f7beb3ec415d9158486ed00c6621e4
LEONARDO VIEIRA GUIMARAES <leonardovieiraxy@hotmail.com>
2023-11-23 22:38:05 -0300

    Changes to be committed: 	modified:   src/components/Skills.tsx

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -100,8 +100,7 @@ export default function Skills() {
             </span>
           </h2>
           <p className="relative text-sm text-gray-700">
-            Professor TI e Matemática | Front-end e Back-end Developer |
-            React.js | React Native | Node.js | TypeScript | JavaScript
+            Professor TI e Matemática | Front-end e Back-end Developer
           </p>
           <div className="absolute left-1/2 top-3 z-0 h-10 w-10 rounded-lg bg-blue-400/10" />
         </div>
